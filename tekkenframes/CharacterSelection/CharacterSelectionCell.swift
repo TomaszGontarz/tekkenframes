cc5351028c56d779fe5721f9893504090de6ee9c
Tomasz Gontarz <ext.tomasz.gontarz@gmail.com>
2017-06-29 15:22:28 +0200

    Add id list to character storage.

--- tekkenframes/CharacterSelection/CharacterSelectionCell.swift
+++ tekkenframes/CharacterSelection/CharacterSelectionCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CharacterSelectionCellTableViewCell: UITableViewCell {
+class CharacterSelectionCell: UITableViewCell {
     @IBOutlet weak var characterImage: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
