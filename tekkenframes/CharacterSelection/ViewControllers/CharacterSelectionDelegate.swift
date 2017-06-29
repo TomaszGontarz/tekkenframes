f76361e1e597d6dc0443040eb94049078242488b
Tomasz Gontarz <ext.tomasz.gontarz@gmail.com>
2017-06-29 14:01:28 +0200

    Connect view controller to the table view.

---
+++ tekkenframes/CharacterSelection/ViewControllers/CharacterSelectionDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  CharacterSelectionDelegate.swift
+//  tekkenframes
+//
+//  Created by Tomasz Gontarz on 29/06/2017.
+//  Copyright © 2017 Tomasz Gontarz. All rights reserved.
+//
+
+import UIKit
+
+class CharacterSelectionDelegate: NSObject, UITableViewDelegate {
+
+}
