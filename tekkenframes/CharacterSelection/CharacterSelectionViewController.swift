378d6474b201675944bf1d3791e47fa73272832f
Tomasz Gontarz <ext.tomasz.gontarz@gmail.com>
2017-06-29 20:04:15 +0200

    Hide status bar.

--- tekkenframes/CharacterSelection/CharacterSelectionViewController.swift
+++ tekkenframes/CharacterSelection/CharacterSelectionViewController.swift
@@ -22,12 +22,16 @@ class CharacterSelectionViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
     
     /*
