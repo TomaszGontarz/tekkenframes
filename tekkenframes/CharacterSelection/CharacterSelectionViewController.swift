eddc16fc5dfbb4b3252e268642ed285724b6ca24
Tomasz Gontarz <ext.tomasz.gontarz@gmail.com>
2017-06-29 19:42:02 +0200

    Hide navigation bar on initial view controller.

--- tekkenframes/CharacterSelection/CharacterSelectionViewController.swift
+++ tekkenframes/CharacterSelection/CharacterSelectionViewController.swift
@@ -20,6 +20,16 @@ class CharacterSelectionViewController: UIViewController {
         characterTableView.delegate = characterDelegate
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
     /*
     // MARK: - Navigation
 
